a722c4cd54aac02fef1c5977ba2295d75c294794
ajian_nedo <asianerd28@gmail.com>
2026-05-09 15:37:40 +0800

    Boilerplate

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    theme: {
+        text: '#e2e1f9',
+        textDim: '#60647a',
+        background: '#252739',
+        secondary: '#1e202f',
+        accent: '#c0a1f0',
+        borderRadius: '10px',
+
+        fontFamily: {
+            mono: ['Source Code Pro', 'monospace']
+        }
+    }
+}
